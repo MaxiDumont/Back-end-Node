cac878ebb7b469a49a80d85e93fd429b9c5f209e
MaxiDumont <maximiliano.dumont@gmail.com>
2022-07-13 12:42:42 +0200

    prueba 10006

--- app.js
+++ app.js
@@ -1,28 +1,14 @@
 'use strict'
 const express = require('express');
-var bodyParser =  require('body-parser');
-const req = require('express/lib/request');
-const cors = require('cors');
-const { default: mongoose } = require('mongoose');
-var mangoose = require('mongoose');
 
 
 const PORT = process.env.PORT || 3999;
 var app = express();
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/api_rest_node', { useNewUrlParser: true })
-        .then (()=>{
-            console.log("conexion a la base de datos establecida satisfactoriamente")
-
-            // Creacion del Servidor
-            app.listen(PORT,()=>{
-                console.log("Servidor corriendo correctamente en la url: localhost:3999")
-            })
-
-        })
-        .catch(err => console.log(err));
-
+// Creacion del Servidor
+app.listen(PORT,()=>{
+    console.log("Servidor corriendo correctamente en la url: localhost:3999")
+})
 
 //ruta de pruba 
 app.get('/prueba',(req,res)=>{
