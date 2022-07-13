54049387fb921684a58ddc86e8d5647254a0fc3f
MaxiDumont <maximiliano.dumont@gmail.com>
2022-07-13 14:25:51 +0200

    prueba 10042

--- app.js
+++ app.js
@@ -23,9 +23,9 @@ mongoose.connect('mongodb://localhost:27017/api_rest_node', { useNewUrlParser: t
         .catch(err => console.log(err));
 
 // archivos de rutas rutas
-// var user_routes = require('./routes/user');
-// var topic_routes = require('./routes/topic');
-// var comment_routes = require('./routes/comment');
+var user_routes = require('./routes/user');
+var topic_routes = require('./routes/topic');
+var comment_routes = require('./routes/comment');
 
 
 //middlewares
@@ -34,9 +34,9 @@ app.use(bodyParser.json());
 app.use(cors());
 
 //reescribir rutas con /api/
-// app.use('/api', user_routes);
-// app.use('/api', topic_routes);
-// app.use('/api', comment_routes);
+app.use('/api', user_routes);
+app.use('/api', topic_routes);
+app.use('/api', comment_routes);
 
 //ruta de pruba 
 app.get('/prueba',(req,res)=>{
