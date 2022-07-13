41ad8430d4396a7b21c932354c12fea66f8d5a22
MaxiDumont <maximiliano.dumont@gmail.com>
2022-07-13 12:48:50 +0200

    prueba 10009

--- app.js
+++ app.js
@@ -1,20 +1,42 @@
-const express = require('express')
-const app = express()
+'use strict'
+const express = require('express');
+var bodyParser =  require('body-parser');
+const req = require('express/lib/request');
+const cors = require('cors');
+const { default: mongoose } = require('mongoose');
+var mangoose = require('mongoose');
+
 const PORT = process.env.PORT || 3999;
-app.use(express.static(__dirname + "/public"));
+var app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/api_rest_node', { useNewUrlParser: true })
+        .then (()=>{
+            console.log("conexion a la base de datos establecida satisfactoriamente")
 
-app.listen(PORT, () => {
-  console.log(`Our app is running on port ${ PORT }`);
-});
+            // Creacion del Servidor
+            app.listen(PORT,()=>{
+                console.log("Servidor corriendo correctamente en la url: localhost:3999")
+            })
 
-app.use((req, res, next) => {
-    // res.status(404).send("Sorry cant find that!");
-    res.status(404).sendFile(__dirname + "/public/404.html");
-  });
+        })
+        .catch(err => console.log(err));
+
+// archivos de rutas rutas
+var user_routes = require('./routes/user');
+var topic_routes = require('./routes/topic');
+var comment_routes = require('./routes/comment');
+
+
+//middlewares
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+app.use(cors());
+
+//reescribir rutas con /api/
+app.use('/api', user_routes);
+app.use('/api', topic_routes);
+app.use('/api', comment_routes);
 
 //ruta de pruba 
 app.get('/prueba',(req,res)=>{
