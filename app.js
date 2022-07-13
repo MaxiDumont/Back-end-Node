26737fb1f7c474884a1ce733ca11d3523b78d656
MaxiDumont <maximiliano.dumont@gmail.com>
2022-07-13 12:44:58 +0200

    prueba 10007

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
-'use strict'
-const express = require('express');
-
-
+const express = require('express')
+const app = express()
 const PORT = process.env.PORT || 3999;
-var app = express();
+app.use(express.static(__dirname + "/public"));
 
-// Creacion del Servidor
-app.listen(PORT,()=>{
-    console.log("Servidor corriendo correctamente en la url: localhost:3999")
+app.get('/', (req, res) => {
+  res.send('Hello World!')
 })
+
+app.listen(PORT, () => {
+  console.log(`Our app is running on port ${ PORT }`);
+});
+
+app.use((req, res, next) => {
+    // res.status(404).send("Sorry cant find that!");
+    res.status(404).sendFile(__dirname + "/public/404.html");
+  });
 
 //ruta de pruba 
 app.get('/prueba',(req,res)=>{
