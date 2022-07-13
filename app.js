1641851adf3440e140279d6647cbdb821f6570ba
MaxiDumont <maximiliano.dumont@gmail.com>
2022-07-13 11:09:24 +0200

    prueba 10003

--- app.js
+++ app.js
@@ -3,8 +3,27 @@ const express = require('express');
 var bodyParser =  require('body-parser');
 const req = require('express/lib/request');
 const cors = require('cors');
+const { default: mongoose } = require('mongoose');
+var mangoose = require('mongoose');
 
+
+const PORT = process.env.PORT || 3999;
 var app = express();
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/api_rest_node', { useNewUrlParser: true })
+        .then (()=>{
+            console.log("conexion a la base de datos establecida satisfactoriamente")
+
+            // Creacion del Servidor
+            app.listen(PORT,()=>{
+                console.log("Servidor corriendo correctamente en la url: localhost:3999")
+            })
+
+        })
+        .catch(err => console.log(err));
+
+
 
 // archivos de rutas rutas
 var user_routes = require('./routes/user');
@@ -27,9 +46,6 @@ app.get('/prueba',(req,res)=>{
     res.status(200).send({message:'Hola mundo desde mi API REST con NodeJS'})
 });
 
-//exportar
-
-module.exports = app;
 
 // //cors Configurar cabeceras y cors
 // app.use((req, res, next) => {
