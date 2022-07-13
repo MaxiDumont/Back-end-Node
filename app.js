c9d1432d12cb6884e714ce1a83cdb8e1b449ae51
MaxiDumont <maximiliano.dumont@gmail.com>
2022-07-13 10:53:19 +0200

    prueba 10000

--- app.js
+++ app.js
@@ -3,11 +3,25 @@ const express = require('express');
 var bodyParser =  require('body-parser');
 const req = require('express/lib/request');
 const cors = require('cors');
+'use strinct'
 
-const PORT = process.env.PORT || 3999;
-app.listen(PORT, () => {
-    console.log(`Our app is running on port ${ PORT }`);
-});
+const { default: mongoose } = require('mongoose');
+var mangoose = require('mongoose');
+var app = require('./app')
+var port = process.env.PORT || 3999;
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/api_rest_node', { useNewUrlParser: true })
+        .then (()=>{
+            console.log("conexion a la base de datos establecida satisfactoriamente")
+
+            // Creacion del Servidor
+            app.listen(port,()=>{
+                console.log("Servidor corriendo correctamente en la url: localhost:3999")
+            })
+
+        })
+        .catch(err => console.log(err));
 
 var app = express();
 
