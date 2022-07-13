e530ce4090584add8f04d2bbd1b5005c618b9ebe
MaxiDumont <maximiliano.dumont@gmail.com>
2022-07-13 13:10:01 +0200

    prueba 10019

--- app.js
+++ app.js
@@ -3,26 +3,16 @@ const express = require('express');
 var bodyParser =  require('body-parser');
 const req = require('express/lib/request');
 const cors = require('cors');
-const { default: mongoose } = require('mongoose');
 // var mangoose = require('mongoose');
 
 const PORT = process.env.PORT || 3999;
 var app = express();
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/api_rest_node', { useNewUrlParser: true })
-        .then (()=>{
-            console.log("conexion a la base de datos establecida satisfactoriamente")
-
-            // Creacion del Servidor
             app.listen(PORT,()=>{
                 console.log("Servidor corriendo correctamente en la url: localhost:3999")
             })
 
-        })
-        .catch(err => console.log(err));
-
-// archivos de rutas rutas
+// archivos de rutas rutasS
 // var user_routes = require('./routes/user');
 // var topic_routes = require('./routes/topic');
 // var comment_routes = require('./routes/comment');
