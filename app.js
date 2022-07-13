e443cc01a8c054a1f4c586a684e6c6ce83baec6d
MaxiDumont <maximiliano.dumont@gmail.com>
2022-07-13 12:40:20 +0200

    prueba 10005

--- app.js
+++ app.js
@@ -24,23 +24,6 @@ mongoose.connect('mongodb://localhost:27017/api_rest_node', { useNewUrlParser: t
         .catch(err => console.log(err));
 
 
-
-// archivos de rutas rutas
-var user_routes = require('./routes/user');
-var topic_routes = require('./routes/topic');
-var comment_routes = require('./routes/comment');
-
-
-//middlewares
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(bodyParser.json());
-app.use(cors());
-
-//reescribir rutas con /api/
-app.use('/api', user_routes);
-app.use('/api', topic_routes);
-app.use('/api', comment_routes);
-
 //ruta de pruba 
 app.get('/prueba',(req,res)=>{
     res.status(200).send({message:'Hola mundo desde mi API REST con NodeJS'})
